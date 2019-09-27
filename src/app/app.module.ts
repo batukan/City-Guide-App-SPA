5e2b44847a480a7d51aab7875994ca6b8c1be586
Emre Batukan <batukan0emre@gmail.com>
2019-09-27 22:45:38 +0300

    Revert "error"
    This reverts commit bdc38e95d9102c3b4e9f51d8dd239027fde6bea3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './src/app/nav/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
