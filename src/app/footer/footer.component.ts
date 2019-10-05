9601742e6619a6a754a4e3e4f7e68d22fd017646
Emre Batukan <batukan0emre@gmail.com>
2019-10-05 16:00:07 +0300

    login and register user services added and footer updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public currentYear: number;
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+
 
   ngOnInit() {
   }
